457b8b57edcf20e5513dda8b5f216f721018648e
Sudeepta Das <sudeepta_das@tamu.edu>
2019-04-19 17:48:26 -0500

    added pictures

--- src/app/destination/destination.component.ts
+++ src/app/destination/destination.component.ts
@@ -11,5 +11,25 @@ export class DestinationComponent implements OnInit {
 
   ngOnInit() {
   }
+   gridChange() {
+    alert("das");
+  //     $('.cat .active').removeClass('active');
+  //     $(this).addClass('active');
+  //     var selector = $(this).attr('data-filter');
+  //     $container.isotope({
+  //         filter: selector,
+  //         animationOptions: {
+  //             duration: 750,
+  //             easing: 'linear',
+  //             queue: false
+  //         }
+  //     });
+  //     return false;
+  };
+  // $('.portfolio-item a').nivoLightbox({
+  //     effect: 'slideDown',
+  //     keyboardNav: true,
+  // });
+
 
 }
