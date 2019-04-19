34e7212efd5486b0e95eb8d49e1d7568b690a973
Sudeepta Das <sudeepta_das@tamu.edu>
2019-04-19 18:10:17 -0500

    added pictures

--- src/assets/js/myFile.js
+++ src/assets/js/myFile.js
@@ -1,19 +1,26 @@
 function gridChange() {
-alert("hello");
-    $('.cat .active').removeClass('active');
-    $(this).addClass('active');
-    var selector = $(this).attr('data-filter');
-    $container.isotope({
-        filter: selector,
-        animationOptions: {
-            duration: 750,
-            easing: 'linear',
-            queue: false
-        }
-    });
-    return false;
+  var $container = $('.portfolio-items');
+  $container.isotope({
+      filter: '*',
+      animationOptions: {
+          duration: 750,
+          easing: 'linear',
+          queue: false
+      }
+  });
+  $('.cat a').click(function() {
+      $('.cat .active').removeClass('active');
+      $(this).addClass('active');
+      var selector = $(this).attr('data-filter');
+      $container.isotope({
+          filter: selector,
+          animationOptions: {
+              duration: 750,
+              easing: 'linear',
+              queue: false
+          }
+      });
+      return false;
+  });
+
 };
-$('.portfolio-item a').nivoLightbox({
-    effect: 'slideDown',
-    keyboardNav: true,
-});
