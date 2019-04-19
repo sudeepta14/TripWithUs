ed8b6f7015a927973198ccaa8b5f887e8c9c11c8
Sudeepta Das <sudeepta_das@tamu.edu>
2019-04-19 17:51:22 -0500

    added pictures

---
+++ src/assets/js/myFile.js
@@ -0,0 +1,19 @@
+function gridChange() {
+  alert("dasfronassets");
+    $('.cat .active').removeClass('active');
+    $(this).addClass('active');
+    var selector = $(this).attr('data-filter');
+    $container.isotope({
+        filter: selector,
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+    return false;
+};
+$('.portfolio-item a').nivoLightbox({
+    effect: 'slideDown',
+    keyboardNav: true,
+});
