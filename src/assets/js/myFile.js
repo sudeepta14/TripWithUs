406bbb0498373501be13696300023c7f69da05d3
Sudeepta Das <sudeepta_das@tamu.edu>
2019-04-19 20:02:36 -0500

    added pictures

--- src/assets/js/myFile.js
+++ src/assets/js/myFile.js
@@ -30,7 +30,6 @@ function w3RemoveClass(element, name) {
   element.className = arr1.join(" ");
 }
 
-
 // Add active class to the current button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
