28a19064fc75664f0dd5ab583908af629fd05fc4
Sudeepta Das <sudeepta_das@tamu.edu>
2019-04-19 23:57:02 -0500

    added pictures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,27 +30,27 @@ import { App, NavController } from 'ionic-angular';
 
 const appRoutes: Routes = [
   {
-    path: 'events',
+    path: '/events',
     component: BookComponent,
     data: { title: 'Book List' }
   },
   {
-    path: 'login',
+    path: '/login',
     component: DestinationListComponent,
     data: { title: 'Login' }
   },
   {
-    path: 'admin',
+    path: '/admin',
     component: AdminComponent,
     data: { title: 'Admin' }
   },
   {
-    path: 'signup',
+    path: '/signup',
     component: DestinationComponent,
     data: { title: 'Sign Up' }
   },
   {
-    path: 'aboutus',
+    path: '/aboutus',
     component: WebFormComponent,
     data: { title: 'Sign Up' }
   },
@@ -71,7 +71,7 @@ const appRoutes: Routes = [
   },
   { path: 'our-story', component: MainPageComponent },
       { path: 'when-where', component: WhenWhereComponent },
-  { path: '',
+  { path: '/',
   component: SampleComponent,
     pathMatch: 'full'
   }
