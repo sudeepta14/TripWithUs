ee0a08dc4161d79b69f0d45b6123348721382390
Sudeepta Das <sudeepta_das@tamu.edu>
2019-04-19 21:11:41 -0500

    added pictures

--- src/assets/js/myFile.js
+++ src/assets/js/myFile.js
@@ -36,7 +36,7 @@ function w3RemoveClass(element, name) {
 
 // Add active class to the current button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
+var btns = btnContainer.getElementsByClassName("");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function(){
     var current = document.getElementsByClassName("active");
