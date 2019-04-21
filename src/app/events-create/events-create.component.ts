ec9dbca07751dce0535d4b2835558387b0cd4b28
Sudeepta Das <sudeepta_das@tamu.edu>
2019-04-20 22:28:06 -0500

    added css

--- src/app/events-create/events-create.component.ts
+++ src/app/events-create/events-create.component.ts
@@ -21,7 +21,7 @@ export class EventsCreateComponent implements OnInit {
     this.http.post('/events', this.events)
       .subscribe(res => {
           let id = res['_id'];
-          this.router.navigate(['', id]);
+          this.router.navigate(['']);
         }, (err) => {
           console.log(err);
         }
