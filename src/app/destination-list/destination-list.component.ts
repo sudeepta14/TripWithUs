ed5d421ad2029b211c19f16579980540cb9b1fb1
Sudeepta Das <sudeepta_das@tamu.edu>
2019-04-21 23:28:48 -0500

    added css

--- src/app/destination-list/destination-list.component.ts
+++ src/app/destination-list/destination-list.component.ts
@@ -31,7 +31,7 @@ export class DestinationListComponent implements OnInit {
   deleteBook(id) {
     this.http.delete('/book/' +id)
       .subscribe(res => {
-        this.router.navigate(['dests']);
+        this.router.navigate(['signup']);
         }, (err) => {
           console.log(err);
         }
