71c832910b218e900a1cc87ae5f9d0476ea38dc5
Sudeepta Das <sudeepta_das@tamu.edu>
2019-04-21 19:45:50 -0500

    added css

--- src/app/destination-list/destination-list.component.ts
+++ src/app/destination-list/destination-list.component.ts
@@ -1,15 +1,35 @@
+
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { tap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: 'destination-list.component.html',
-  styleUrls: ['destination-list.component.css']
+  selector: 'app-book',
+  templateUrl: './destination-list.component.html',
+  styleUrls: ['./destination-list.component.css']
 })
 export class DestinationListComponent implements OnInit {
 
-  constructor() { }
+  books: any;
+
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
+    this.http.get('/book').subscribe(data => {
+      console.log(data);
+      this.books = data;
+    });
+  }
+
+
+  logout() {
+    localStorage.removeItem('jwtToken');
+    this.router.navigate(['login']);
   }
 
 }
+
+
