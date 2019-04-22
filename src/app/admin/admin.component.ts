9d6b1757119d286f14eb8862174f9f9981299af8
Sudeepta Das <sudeepta_das@tamu.edu>
2019-04-22 01:27:50 -0500

    added css

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from "@angular/router";
+import { Observable } from 'rxjs/Observable';
+import { tap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +12,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
+  }
+  logout() {
+    this.router.navigate(['dests']);
   }
 
 }
